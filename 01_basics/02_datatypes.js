9097939fff4c3995b06b459d7d73ebdc0255aeb2
Suchetana Mukherjee <113498863+Suchetana2001@users.noreply.github.com>
2024-05-08 15:32:56 +0000

    datatype

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,23 @@
 "use strict";//treat all js code as new version
-//alert("hello")
+
+//alert("hello")// we are using nodejs,noy browser
+
+console.log(3+3)//code readability should be high
+console.log("manta")
+
+let name="manta"
+let age=22
+let isLoggedIn = false
+let state;
+
+//number => 2^53
+//bigInt
+//string=>""
+//boolean => true/false
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+//object
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
