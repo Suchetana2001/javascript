8978fa3d2662a6b15a8152fa3989a679dd21e28c
Suchetana Mukherjee <113498863+Suchetana2001@users.noreply.github.com>
2024-05-11 17:53:02 +0000

    operation done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,3 +21,25 @@ let stringNum=String(num)
 
 console.log(stringNum)
 console.log(typeof stringNum);
+
+//*************operation ***************
+let val=3
+let negVal=-val;
+console.log(negVal);
+
+// console.log(3+3)
+// console.log(3-3);
+// console.log(3*3);
+// console.log(3**3);
+// console.log(3/3);
+// console.log(3%3);
+
+let s1="hello"
+let s2=" manta"
+let s3=s1+s2
+console.log(s3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"1"+2);
