8cbcf0362dcc68f85775100735ce71808b81ea8e
Suchetana Mukherjee <113498863+Suchetana2001@users.noreply.github.com>
2024-05-11 14:50:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = 33;
+let valueInNumber = Number(score)
+
+console.log(typeof(score)) //for type check
+//console.log(typeof score) //for type check
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+//convert to number
+//"33"=>33
+//"33abc"=>NaN
+//true=>1 false=>0
+//null=>0
+//undefined=>NaN
+let isLoggedIn=1;
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn)
+//1=>true 0=>false
+
+let num=33 
+let stringNum=String(num)
+
+console.log(stringNum)
+console.log(typeof stringNum);
